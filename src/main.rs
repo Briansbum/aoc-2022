3e5a633fb1e7990ce572c4d6b791ad0a7113463f
Alex Freestone <freestone.alex@gmail.com>
2023-05-31 10:04:04 +0100

    day2_2 - more pattern matching goodness

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("day1_1: {}", day1_1("src/data/day1.txt"));
     println!("day1_2: {}", day1_2("src/data/day1.txt"));
     println!("day2_1: {}", day2_1("src/data/day2.txt"));
+    println!("day2_2: {}", day2_2("src/data/day2.txt"));
 }
 
 fn day1_2(filename: &str) -> usize {
@@ -29,19 +30,48 @@ fn day1_1(filename: &str) -> usize {
         })
 }
 
-fn day2_1(filename: &str) -> usize {
+fn day2_2(filename: &str) -> usize {
     let f = utils::readfile(filename);
     f.lines()
         .map(|l| l.split_whitespace().collect())
         .map(|v: Vec<&str>| {
-            let my_play = Play::from_str(v[1]).unwrap();
-            match Play::from_str(v[0]).unwrap().cmp(&my_play) {
-                Ordering::Equal => my_play as usize + 3,
-                Ordering::Greater => my_play as usize, 
-                Ordering::Less => my_play as usize+ 6, 
+            let them = Play::from_str(v[0]).unwrap();
+            let me = Play::from_str(v[1]).unwrap();
+            match me {
+                Play::Paper => match them {
+                    Play::Paper => play_game(them, Play::Paper),
+                    Play::Rock => play_game(them, Play::Rock),
+                    Play::Scissors => play_game(them, Play::Scissors),
+                },
+                Play::Rock => match them {
+                    Play::Paper => play_game(them, Play::Rock),
+                    Play::Rock => play_game(them, Play::Scissors),
+                    Play::Scissors => play_game(them, Play::Paper),
+                },
+                Play::Scissors => match them {
+                    Play::Paper => play_game(them, Play::Scissors),
+                    Play::Rock => play_game(them, Play::Paper),
+                    Play::Scissors => play_game(them, Play::Rock),
+                },
             }
         })
         .fold(0, |acc, v| acc + v)
+}
+
+fn day2_1(filename: &str) -> usize {
+    let f = utils::readfile(filename);
+    f.lines()
+        .map(|l| l.split_whitespace().collect())
+        .map(|v: Vec<&str>| play_game(Play::from_str(v[0]).unwrap(), Play::from_str(v[1]).unwrap()))
+        .fold(0, |acc, v| acc + v)
+}
+
+fn play_game(l: Play, r: Play) -> usize {
+    match l.cmp(&r) {
+        Ordering::Equal => r as usize + 3,
+        Ordering::Greater => r as usize,
+        Ordering::Less => r as usize + 6,
+    }
 }
 
 #[derive(Eq)]
